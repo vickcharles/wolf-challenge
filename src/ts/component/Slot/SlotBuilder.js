eb86b495422af6a43f9f703a681497fbd9a33fd6
vickler charles <charlesvikler@gmail.com>
2019-09-04 10:18:59 -0500

    added material-ui dialog and component to build an slot

---
+++ src/ts/component/Slot/SlotBuilder.js
@@ -0,0 +1,13 @@
+import * as React from 'react';
+
+class SlotBuilder extends React.Component {
+	render() {
+    return (
+			<div>
+				<h1>slot builder goes here</h1>
+			</div>
+		);
+	}
+};
+
+export default SlotBuilder;
--- src/ts/component/common/Header.js
+++ src/ts/component/common/Header.js
@@ -5,12 +5,17 @@ import {
   Toolbar,
   Typography,
   Button,
+  Dialog,
+  DialogTitle,
   Container
 } from '@material-ui/core/';
 
 import '../../../assets/scss/Header.scss';
+import SlotBuilder from '../../component/Slot/SlotBuilder';
 
 const Header = () => {
+  const [open, setOpen] = React.useState();
+
   return (
     <div>
       <AppBar position="static" className="app-bar">
@@ -20,7 +25,12 @@ const Header = () => {
               WOLF CHALLENGE
             </Typography>
             <div>
-              <Button variant="contained" color="secondary" className="margin-right-xsmall">
+              <Button
+                onClick={() => setOpen(true)}
+                variant="contained"
+                color="secondary"
+                className="margin-right-xsmall"
+              >
                 Post a job
               </Button>
               <Button color="inherit">
@@ -30,6 +40,18 @@ const Header = () => {
           </Toolbar>
         </Container>
       </AppBar>
+      <Dialog
+        maxWidth="sm"
+        fullWidth={true}
+        open={open}
+        keepMounted
+        onClose={() => setOpen(false)}
+        aria-labelledby="alert-dialog-slide-title"
+        aria-describedby="alert-dialog-slide-description"
+      >
+        <DialogTitle id="alert-dialog-slide-title">CREATE NEW SLOT</DialogTitle>
+        <SlotBuilder />
+      </Dialog>
     </div>
   );
 };
