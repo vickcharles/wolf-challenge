af5041f4005b8e2a07d90b54d16983ec76c34f55
vickler charles <charlesvikler@gmail.com>
2019-09-04 10:19:22 -0500

    more editions

--- src/ts/component/Slot/SlotBuilder.js
+++ src/ts/component/Slot/SlotBuilder.js
@@ -1,10 +1,12 @@
 import * as React from 'react';
 
+import { Container } from '@material-ui/core/';
+
 class SlotBuilder extends React.Component {
 	render() {
     return (
 			<div>
-				<h1>slot builder goes here</h1>
+				<h1>Slot builder goes here</h1>
 			</div>
 		);
 	}
