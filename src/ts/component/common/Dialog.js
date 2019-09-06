f3c42bba513f70c9003593236680f292c3aca1ef
vickler charles <charlesvikler@gmail.com>
2019-09-06 09:44:16 -0500

    added conditionals to set fullscreen dialog

--- src/ts/component/common/Dialog.js
+++ src/ts/component/common/Dialog.js
@@ -3,11 +3,26 @@ import * as React from 'react';
 import { Dialog, DialogTitle, Container} from '@material-ui/core/';
 
 const CommonDialog = (props) => {
+
+  const isSmallScreen = () => typeof document !== "undefined" ? document.body.clientWidth < 600: false;
+  const [fullScreen, setFullScreen] = React.useState(isSmallScreen);
+
+  React.useEffect(() => {
+    function checkScreenSize() {
+      setFullScreen(isSmallScreen());
+    }
+
+    window.addEventListener("resize", checkScreenSize);
+
+    return () => window.removeEventListener("resize", checkScreenSize);
+  });
+
   return (
     <Dialog
       maxWidth={props.maxWidth}
       fullWidth={true}
       className="dialog"
+      fullScreen={fullScreen}
       open={props.open}
       scroll="body"
       onClose={props.onClose}
